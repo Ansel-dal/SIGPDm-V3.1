790f423f2f9db3c76b78d77efa39f4b2eb7d58dd
HP <HP@DESKTOP-UE98ED7>
2023-02-16 13:40:37 -0300

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Shared/Models/Insumo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Shared.Models
+{
+    public class Insumo
+    {
+        public int Id { get; set; }
+
+        [StockMin]//No es la mejor forma de resolverlo
+        public int StockMin { get; set; }
+
+        [StockMax] //No es la mejor forma de resolverlo
+        public int StockMax { get; set; }
+
+        [StockReal]
+        public int? StockReal { get; set; }
+        public string? Nombre { get; set; }
+        public string? Codigo { get; set; }
+        public string? Foto { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Recepcion { get; set; }
+
+
+
+
+
+    }
+
+    public class StockMinAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var insumo = (Insumo)validationContext.ObjectInstance;
+
+            if (insumo.StockMin >= 0)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("El stock mínimo debe ser mayor o igual a cero");
+        }
+    }
+
+    public class StockMaxAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var insumo = (Insumo)validationContext.ObjectInstance;
+
+            if (insumo.StockMax > insumo.StockMin)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("El stock máximo debe ser mayor al stock mínimo");
+        }
+    }
+
+    public class StockRealAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var insumo = (Insumo)validationContext.ObjectInstance;
+
+            if (insumo.StockReal >= 0)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("El stock real debe ser mayor o igual a cero");
+        }
+    }
+
+
+}
---
+++ BlazorApp1/Shared/Models/Ordencompra.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Shared.Models
+{
+    public partial class Ordencompra
+    {
+        public int Id { get; set; }
+        public string? Estado { get; set; }
+        public string? Especificacion { get; set; }
+        public string? Archivo { get; set; }
+        public DateTime? Aprobada { get; set; }
+        public DateTime? Generada { get; set; }
+        public DateTime? Recepcionada { get; set; }
+
+        [Cantidad]
+        public int? Cantidad { get; set; }
+        public int? Insumo { get; set; }
+        public int? Proveedor { get; set; }
+
+        [Precio]
+        public int? Precio { get; set; } //Cambiar de int a float porque el precio puede tener decimales
+        public string? CondicionPago { get; set; }
+    }
+
+    public class CantidadAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var orden = (Ordencompra)validationContext.ObjectInstance;
+
+            if (orden.Cantidad >= 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("La cantidad pedida debe ser mayor o igual a 1");
+        }
+    }
+
+    public class PrecioAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var orden = (Ordencompra)validationContext.ObjectInstance;
+
+            if (orden.Precio !=null && orden.Precio >= 0)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult("El precio debe ser mayor o igual a 0");
+        }
+    }
+
+}
